527d2283bce2317c08a5e32ba9ea0694e432e806
brettbookbinder <78259304+brettbookbinder@users.noreply.github.com>
2021-05-09 15:39:25 -0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -4,5 +4,5 @@ const dbpass = 'pass'
 const port = process.env.PORT|5432
 
 //const client = new Client({ port: port, host: 'localhost', user: dbuser, password: dbpass, database: "imagequiz" });
-const client = new Client({ connectionString: process.env.DATABASE_URL})
+const client = new Client({ connectionString: process.env.DATABASE_URL, port: port})
 module.exports = client;
